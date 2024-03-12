a222c963ee42de0d00fba9370016f41063bc61fc
barbarahellmann <babshellmann@web.de>
2024-03-12 11:46:43 +0100

    Hello World in ASCII

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Kind of a perfect world!");
+    public static void main(String[] args) throws IOException {
+        String asciiArt1 = FigletFont.convertOneLine("Hello World");
+        System.out.println(asciiArt1);
     }
+
 }
