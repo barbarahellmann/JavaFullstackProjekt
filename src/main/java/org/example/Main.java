c8de105a2477cebba3404fcb2e6378a153be0984
barbarahellmann <babshellmann@web.de>
2024-03-12 11:32:32 +0100

    Forgot pull

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello perfect world!");
+        System.out.println("Kind of a perfect world!");
     }
 }
